8dae4c8cf7d69ee22db946bbe45cae6600e108d4
newExpand <magin0auto@gmail.com>
2023-03-26 22:06:55 +0900

    feat: useState 사용해보기

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle("업데이트");
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} month={props.month} year={props.year} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}만원</div>
             </div>
-            <button>제목 수정</button>
+            <button onClick={clickHandler}>제목 수정</button>
         </Card>
     );
 };
