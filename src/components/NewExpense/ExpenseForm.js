fe22095d03a91565f7008f318ef9f310bab5ef96
newExpand <magin0auto@gmail.com>
2023-03-27 00:20:12 +0900

    feat: 입력폼 컴포넌트 추가하기

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>제목</label>
+                    <input type="text" />
+                </div>
+                <div className="new-expense__control">
+                    <label>금액</label>
+                    <input type="number" min="1" step="1" />
+                </div>
+                <div className="new-expense__control">
+                    <label>날짜</label>
+                    <input type="date" min="2019-01-01" max="2023-12-31" />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">추가하기</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
