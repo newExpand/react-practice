33eddb6adfcc519c835fd84fc002aa8a98c69a9b
newExpand <magin0auto@gmail.com>
2023-03-27 08:59:50 +0900

    feat: 폼에 데이터 입력후 추가하기 버튼 눌를시 실제 아이템 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        title: "화장실 휴지",
+        amount: 94,
+        date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "72인치 티비", amount: 799, date: new Date(2021, 2, 12) },
+    {
+        id: "e3",
+        title: "차 보험",
+        amount: 294,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: "e4",
+        title: "목재 책상",
+        amount: 450,
+        date: new Date(2021, 5, 3),
+    },
+];
+
 const App = () => {
-    const expenses = [
-        {
-            id: "e1",
-            title: "화장실 휴지",
-            amount: 94,
-            date: new Date(2020, 7, 14),
-        },
-        { id: "e2", title: "72인치 티비", amount: 799, date: new Date(2021, 2, 12) },
-        {
-            id: "e3",
-            title: "차 보험",
-            amount: 294,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: "e4",
-            title: "목재 책상",
-            amount: 450,
-            date: new Date(2021, 5, 3),
-        },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = (expense) => {
-        console.log(expense);
+        setExpenses((preExpenses) => {
+            return [expense, ...preExpenses];
+        });
     };
 
     return (
