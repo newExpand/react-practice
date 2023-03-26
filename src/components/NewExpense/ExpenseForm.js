0b820dab5723abaf5600e59c71dfbff8b5f69817
newExpand <magin0auto@gmail.com>
2023-03-27 00:55:13 +0900

    feat : useState 여러개 사용해 보기

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    const titleChangeHandler = () => {};
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value);
+    };
+
+    const amountChangeHandler = (e) => {
+        setEnteredAmount(e.target.value);
+    };
+
+    const dateChangeHandler = (e) => {
+        setEnteredDate(e.target.value);
+    };
 
     return (
         <form>
@@ -13,11 +27,11 @@ const ExpenseForm = () => {
                 </div>
                 <div className="new-expense__control">
                     <label>금액</label>
-                    <input type="number" min="1" step="1" />
+                    <input type="number" min="1" step="1" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>날짜</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31" />
+                    <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
