5fbee19058e2c0f441330c90c774007c1ef0d9ce
newExpand <magin0auto@gmail.com>
2023-03-26 20:38:53 +0900

    feat: children 속성을 통한 합성 시도

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} month={props.month} year={props.year} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}만원</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
