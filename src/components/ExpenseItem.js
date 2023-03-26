2b5f0d6a3d9cc27ca335ac314b7d07302e3a3913
newExpand <magin0auto@gmail.com>
2023-03-26 18:13:19 +0900

    feat: css 스타일 추가

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-    return <h2>ExpenseItem</h2>;
+    return (
+        <div className="expense-item">
+            <div>2023년 3월 28일</div>
+            <div className="expense-item__description">
+                <h2>차 보험</h2>
+                <div className="expense-item__price">29만원</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
