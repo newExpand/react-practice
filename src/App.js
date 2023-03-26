118ba220e60f98f2ab2e5a941a80f8cc79675aa7
newExpand <magin0auto@gmail.com>
2023-03-26 18:41:37 +0900

    feat: props를 통해  데이터 전달하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {
+            id: "e1",
+            title: "화장실 휴지",
+            amount: 94,
+            date: new Date(2020, 7, 14),
+        },
+        { id: "e2", title: "72인치 티비", amount: 799, date: new Date(2021, 2, 12) },
+        {
+            id: "e3",
+            title: "차 보험",
+            amount: 294,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: "e4",
+            title: "목재 책상",
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
     return (
         <div>
             <h2>시작해보자구~!</h2>
-            <ExpenseItem />
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
         </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 2, 28);
-    const expenseTitle = "차 보험";
-    const expenseAmount = 29;
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}만원</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}만원</div>
             </div>
         </div>
     );
