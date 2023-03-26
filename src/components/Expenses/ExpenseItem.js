69951b017c635d6dbcd7770f65bb2ff20ef32d86
newExpand <magin0auto@gmail.com>
2023-03-27 08:45:09 +0900

    feat: 데이터와 map메서드를 활용한 컴포넌트 렌더링

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,13 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle("업데이트");
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} month={props.month} year={props.year} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}만원</div>
             </div>
-            <button onClick={clickHandler}>제목 수정</button>
         </Card>
     );
 };
