0083fb026a283a0ea34f22bbec3781134358dce4
newExpand <magin0auto@gmail.com>
2023-03-27 16:37:43 +0900

    style: key값 추가 및 타입 변경

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -3,18 +3,18 @@ import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
     const chartDataPoints = [
-        { label: "01월", value: 0 },
-        { label: "02월", value: 0 },
-        { label: "03월", value: 0 },
-        { label: "04월", value: 0 },
-        { label: "05월", value: 0 },
-        { label: "06월", value: 0 },
-        { label: "07월", value: 0 },
-        { label: "08월", value: 0 },
-        { label: "09월", value: 0 },
-        { label: "10월", value: 0 },
-        { label: "11월", value: 0 },
-        { label: "12월", value: 0 },
+        { id: "d0", label: "01월", value: 0 },
+        { id: "d1", label: "02월", value: 0 },
+        { id: "d2", label: "03월", value: 0 },
+        { id: "d3", label: "04월", value: 0 },
+        { id: "d4", label: "05월", value: 0 },
+        { id: "d5", label: "06월", value: 0 },
+        { id: "d6", label: "07월", value: 0 },
+        { id: "d7", label: "08월", value: 0 },
+        { id: "d8", label: "09월", value: 0 },
+        { id: "d9", label: "10월", value: 0 },
+        { id: "d10", label: "11월", value: 0 },
+        { id: "d11", label: "12월", value: 0 },
     ];
 
     for (const expense of props.expenses) {
