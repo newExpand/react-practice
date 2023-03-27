ad414e8168476f4852ffdc2c710514c2d1827f34
newExpand <magin0auto@gmail.com>
2023-03-27 09:16:12 +0900

    feat: 연도 필터 기능 추가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,11 +11,15 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
                 ))}
             </Card>
