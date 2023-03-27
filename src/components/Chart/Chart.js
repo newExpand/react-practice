82a551e864371b87dedc2f4df9a8020ba6abe77a
newExpand <magin0auto@gmail.com>
2023-03-27 15:58:20 +0900

    feat: 차트 데이터 연결 및 완성

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,10 +3,13 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
+
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => (
-                <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={null} label={dataPoint.label} />
+                <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />
             ))}
         </div>
     );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import Card from "../UI/Card";
-import "./Expenses.css";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2020");
@@ -19,6 +20,7 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
