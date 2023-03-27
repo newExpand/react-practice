68acf4c4ee3d32242b75eafbf2f4e50be1b44131
newExpand <magin0auto@gmail.com>
2023-03-27 15:16:40 +0900

    feat : 동적 스타일 추가하기

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./ChartBar.css";
 
-const ChartBar = () => {
-    return <div>ChartBar</div>;
+const ChartBar = (props) => {
+    let barFillHeight = "0%";
+
+    if (props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fil" style={{ height: barFillHeight }}></div>
+            </div>
+            <div className="chart-bar__labe">{props.label}</div>
+        </div>
+    );
 };
+
+{
+    /* <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={null} label={dataPoint.label} />; */
+}
 
 export default ChartBar;
