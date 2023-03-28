0b8001c66e259151f5b1a99a7e3edf355190710b
newExpand <magin0auto@gmail.com>
2023-03-28 18:39:17 +0900

    feat: 유효성 검증 추가하기

--- 01-starting-project-4/src/components/Users/AddUser.js
+++ 01-starting-project-4/src/components/Users/AddUser.js
@@ -9,6 +9,15 @@ const AddUser = (props) => {
 
     const addUserHandler = (e) => {
         e.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        setEnteredUsername("");
+        setEnteredAge("");
     };
 
     const usernameChangeHandler = (e) => {
@@ -23,9 +32,9 @@ const AddUser = (props) => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">이름</label>
-                <input type="text" id="username" onChange={usernameChangeHandler} />
+                <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler} />
                 <label htmlFor="age">나이</label>
-                <input type="number" id="age" onChange={ageChangeHandler} />
+                <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler} />
                 <Button type="submit">유저 추가하기</Button>
             </form>
         </Card>
