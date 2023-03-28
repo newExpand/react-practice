30c76836d8a7b9bb928d8cd065f84101ffaf1292
newExpand <magin0auto@gmail.com>
2023-03-28 13:50:30 +0900

    feat: 재사용 가능한 카드 컴포넌트 추가

--- 01-starting-project-4/src/components/Users/AddUser.js
+++ 01-starting-project-4/src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
     const addUserHandler = (e) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
     };
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">이름</label>
-            <input type="text" id="username" />
-            <label htmlFor="age">나이</label>
-            <input type="text" id="age" />
-            <button type="submit">유저 추가하기</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">이름</label>
+                <input type="text" id="username" />
+                <label htmlFor="age">나이</label>
+                <input type="text" id="age" />
+                <button type="submit">유저 추가하기</button>
+            </form>
+        </Card>
     );
 };
 
