091e32ea298d2d3478bb0808c96e5b6291098607
newExpand <magin0auto@gmail.com>
2023-03-28 18:31:50 +0900

    feat: 사용자 인풋 state 추가

--- 01-starting-project-4/src/components/Users/AddUser.js
+++ 01-starting-project-4/src/components/Users/AddUser.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
     const addUserHandler = (e) => {
         e.preventDefault();
+    };
+
+    const usernameChangeHandler = (e) => {
+        setEnteredUsername(e.target.value);
+    };
+
+    const ageChangeHandler = (e) => {
+        setEnteredAge(e.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">이름</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" onChange={usernameChangeHandler} />
                 <label htmlFor="age">나이</label>
-                <input type="text" id="age" />
+                <input type="number" id="age" onChange={ageChangeHandler} />
                 <Button type="submit">유저 추가하기</Button>
             </form>
         </Card>
