4aa1ef40f024070bbcecc76be5edf2929f7e1fde
newExpand <magin0auto@gmail.com>
2023-03-29 00:44:37 +0900

    feat: 오류 모달 state 관리

--- 01-starting-project-4/src/components/Users/AddUser.js
+++ 01-starting-project-4/src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
+    const [error, setError] = useState();
 
     const addUserHandler = (e) => {
         e.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "올바르지 않은 값 입니다.",
+                message: "이름과 나이를 정확히 입력해주세요.",
+            });
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: "잘못된 나이 입니다.",
+                message: "1이상의 나이를 입력해 주세요.ㄴ",
+            });
             return;
         }
 
@@ -30,9 +39,13 @@ const AddUser = (props) => {
         setEnteredAge(e.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
         <div>
-            <ErrorModal title="에러 발생" message="무언가 단단히 잘못됨" />
+            {error && <ErrorModal onConfirm={errorHandler} title={error.title} message={error.message} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">이름</label>
