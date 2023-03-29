4e2f749654b3984cbad1c7fcd4a19c7a79c536fd
newExpand <magin0auto@gmail.com>
2023-03-29 18:56:52 +0900

    feat: useReducer 와 useEffect 함께 사용하기

--- 06-usereducer-starting-project/src/components/Login/Login.js
+++ 06-usereducer-starting-project/src/components/Login/Login.js
@@ -53,32 +53,33 @@ const Login = (props) => {
         };
     }, []);
 
-    // useEffect(() => {
-    //   const identifier = setTimeout(() => {
-    //     console.log('Checking form validity!');
-    //     setFormIsValid(
-    //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    //     );
-    //   }, 500);
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
 
-    //   return () => {
-    //     console.log('CLEANUP');
-    //     clearTimeout(identifier);
-    //   };
-    // }, [enteredEmail, enteredPassword]);
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log("Checking form validity!");
+            setFormIsValid(emailIsValid && passwordIsValid);
+        }, 300);
+
+        return () => {
+            console.log("CLEANUP");
+            clearTimeout(identifier);
+        };
+    }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
         // setEnteredEmail(event.target.value);
         dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-        setFormIsValid(event.target.value.includes("@") && passwordState.value.trim().length > 6);
+        // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
     };
 
     const passwordChangeHandler = (event) => {
         // setEnteredPassword(event.target.value);
         dispatchPassword({ type: "USER_PW_INPUT", val: event.target.value });
 
-        setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+        // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
     };
 
     const validateEmailHandler = () => {
