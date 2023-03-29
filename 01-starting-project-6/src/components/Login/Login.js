4c77447e18392c4987faeffacbe95e9a444c5cdd
newExpand <magin0auto@gmail.com>
2023-03-29 13:17:33 +0900

    feat: useEffect cleanup함수 사용하기

--- 01-starting-project-6/src/components/Login/Login.js
+++ 01-starting-project-6/src/components/Login/Login.js
@@ -12,7 +12,14 @@ const Login = (props) => {
     const [formIsValid, setFormIsValid] = useState(false);
 
     useEffect(() => {
-        setFormIsValid(enteredEmail.includes("@") && enteredPassword.trim().length > 6);
+        const identifier = setTimeout(() => {
+            console.log("확인용!~");
+            setFormIsValid(enteredEmail.includes("@") && enteredPassword.trim().length > 6);
+        }, 300);
+
+        return () => {
+            clearTimeout(identifier);
+        };
     }, [enteredEmail, enteredPassword]);
 
     const emailChangeHandler = (event) => {
