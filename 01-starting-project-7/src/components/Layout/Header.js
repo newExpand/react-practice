81b5519f95ddc6bd8f1af6d71452c09f8f5d1241
newExpand <magin0auto@gmail.com>
2023-03-30 20:26:24 +0900

    feat: 장바구니 버튼 컴포넌트 추가

--- 01-starting-project-7/src/components/Layout/Header.js
+++ 01-starting-project-7/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>리액트 맛집</h1>
-                <button>장바구니</button>
+                <HeaderCartButton />
             </header>
             <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="" />
