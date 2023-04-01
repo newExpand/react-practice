4cb32b56944f90bc35be3cfe6bb65651e85a1cf2
newExpand <magin0auto@gmail.com>
2023-04-01 20:18:56 +0900

    Comment : 컴포넌트 업데이트시 동작원리 작성 Test: 컴포넌트 업데이트 동작원리에 대한 테스트 코드

--- 01-starting-setup-8/src/App.js
+++ 01-starting-setup-8/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    /* 동작원리
+
+    state 변경이나 props 변경, 또는 컨텍스트 변경은 컴포넌트 함수를 다시 실행시킨다.
+    그 사실은 클릭시 state가 변경되는 발작버튼을 클릭함으로써 console.log("실행중")이 매번 콘솔창에 찍히는것으로 확인할 수 있다.
+
+    */
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    console.log("실행중");
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    };
+
+    return (
+        <div className="app">
+            <h1>헤이 안뇽안뇽!</h1>
+            {showParagraph && <p>발작한 글입니다~!</p>}
+            <Button onClick={toggleParagraphHandler}>발작버튼</Button>
+        </div>
+    );
 }
 
 export default App;
