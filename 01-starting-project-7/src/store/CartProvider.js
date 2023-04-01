61c31e04c5dd2f3142fb57d9733d9bd35df7f06d
newExpand <magin0auto@gmail.com>
2023-04-01 16:57:03 +0900

    feat: 장바구니 마이너스 버튼 클릭시 숫자 줄거나 아이템 사라지는 기능 추가

--- 01-starting-project-7/src/store/CartProvider.js
+++ 01-starting-project-7/src/store/CartProvider.js
@@ -31,7 +31,25 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount,
         };
     }
+
     if (action.type === "REMOVE") {
+        const existingItemIndex = state.items.findIndex((item) => item.id === action.id);
+        const existingCartItem = state.items[existingItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
     }
     return defaultCartState;
 };
