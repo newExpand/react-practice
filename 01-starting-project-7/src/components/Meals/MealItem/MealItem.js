263ed752b33775a49c317421a040a992e87edc1c
newExpand <magin0auto@gmail.com>
2023-04-01 09:03:12 +0900

    feat: Refs 와 fowardRefs를 작업(추가하기 버튼 눌렀을 시 개수에 비례해 장바구니 추가

--- 01-starting-project-7/src/components/Meals/MealItem/MealItem.js
+++ 01-starting-project-7/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
     const price = `${props.price.toLocaleString()}원`;
+    const cartCtx = useContext(CartContext);
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount,
+            price: props.price,
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -13,7 +24,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
--- 01-starting-project-7/src/components/Meals/MealItem/MealItemForm.js
+++ 01-starting-project-7/src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
+
     return (
-        <form className={classes.form}>
-            <Input label="개수" input={{ id: "amount_" + props.id, type: "number", min: "1", max: "5", step: "1", defaultValue: "1" }} />
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input ref={amountInputRef} label="개수" input={{ id: "amount_" + props.id, type: "number", min: "1", max: "5", step: "1", defaultValue: "1" }} />
             <button>추가하기</button>
+            {!amountIsValid && <p>1에서 5사이의 값만 입력 가능합니다.</p>}
         </form>
     );
 };
