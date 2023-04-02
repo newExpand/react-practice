4d50f5682ecbcf1527173b861e3cb13692dcaf50
newExpand <magin0auto@gmail.com>
2023-04-03 03:07:29 +0900

    feat: 양식 제출 처리 및 사용자  입력 값 가져오기

--- 01-starting-project-12/src/components/SimpleInput.js
+++ 01-starting-project-12/src/components/SimpleInput.js
@@ -1,15 +1,42 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const nameInputRef = useRef();
+    const [enteredName, setEnteredName] = useState("");
+
+    const nameInputChangeHandler = (e) => {
+        setEnteredName(e.target.value);
+    };
+
+    const formSubmissionHandler = (e) => {
+        e.preventDefault();
+
+        console.log(enteredName);
+        const enteredValue = nameInputRef.current.value;
+
+        console.log(enteredValue);
+
+        nameInputRef.current.value = "";
+        setEnteredName("");
+    };
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className="form-control">
+                <label htmlFor="name">Your Name</label>
+                <input
+                    ref={nameInputRef}
+                    value={enteredName}
+                    type="text"
+                    id="name"
+                    onChange={nameInputChangeHandler}
+                />
+            </div>
+            <div className="form-actions">
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
