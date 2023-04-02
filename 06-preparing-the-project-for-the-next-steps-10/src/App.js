214f6e93eab17d503110d989583ab38e8685e5e4
newExpand <magin0auto@gmail.com>
2023-04-02 17:07:37 +0900

    feat: post 요청 보내기

--- 06-preparing-the-project-for-the-next-steps-10/src/App.js
+++ 06-preparing-the-project-for-the-next-steps-10/src/App.js
@@ -22,15 +22,18 @@ function App() {
 
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -41,8 +44,18 @@ function App() {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
 
-    function addMovieHandler(movie) {
-        console.log(movie);
+    async function addMovieHandler(movie) {
+        const response = await fetch(
+            "https://react-http-f8c0c-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(movie),
+            }
+        );
+
+        const data = await response.json();
+        console.log(data);
     }
 
     let content = <p>Found no movies.</p>;
