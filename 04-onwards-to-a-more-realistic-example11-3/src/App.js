03f8b48c178cecb6da192b4808694d325124df68
newExpand <magin0auto@gmail.com>
2023-04-03 00:33:14 +0900

    feat: 사용자 정의 훅 useHttp훅 사용 및 로직 조정하기

--- 04-onwards-to-a-more-realistic-example11-3/src/App.js
+++ 04-onwards-to-a-more-realistic-example11-3/src/App.js
@@ -2,20 +2,40 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+    const [tasks, setTasks] = useState([]);
+
+    const transformTasks = (tasksObj) => {
+        const loadedTasks = [];
+
+        for (const key in tasksObj) {
+            loadedTasks.push({ id: key, text: tasksObj[key].text });
+        }
+
+        setTasks(loadedTasks);
+    };
+
+    const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
     useEffect(() => {
-        // fetchTasks();
-    }, []);
+        fetchTasks(
+            {
+                url: "https://react-http-f8c0c-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+            },
+            transformTasks
+        );
+    }, [fetchTasks]);
 
     const taskAddHandler = (task) => {
-        // setTasks((prevTasks) => prevTasks.concat(task));
+        setTasks((prevTasks) => prevTasks.concat(task));
     };
 
     return (
         <React.Fragment>
             <NewTask onAddTask={taskAddHandler} />
-            {/* <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} /> */}
+            <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
         </React.Fragment>
     );
 }
