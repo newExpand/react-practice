c50f0f818d0b006d713eaaa20828132c2a8f3ef8
newExpand <magin0auto@gmail.com>
2023-04-03 09:18:36 +0900

    refactor: 전체 form 유효성 관리하기

--- 01-starting-project-12/src/components/SimpleInput.js
+++ 01-starting-project-12/src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
     const [enteredName, setEnteredName] = useState("");
@@ -6,6 +6,12 @@ const SimpleInput = (props) => {
 
     const enteredNameIsValid = enteredName.trim() !== "";
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid) {
+        formIsValid = true;
+    }
 
     const nameInputChangeHandler = (e) => {
         setEnteredName(e.target.value);
@@ -45,7 +51,7 @@ const SimpleInput = (props) => {
                 {nameInputIsInvalid && <p className="error-text">이름이 비어있습니다.</p>}
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
         </form>
     );
