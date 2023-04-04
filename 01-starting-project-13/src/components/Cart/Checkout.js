c0a7244883633094b963851961418716aeb035d0
newExpand <magin0auto@gmail.com>
2023-04-04 11:48:24 +0900

    feat: useRef를 이용한 양식값 읽기

--- 01-starting-project-13/src/components/Cart/Checkout.js
+++ 01-starting-project-13/src/components/Cart/Checkout.js
@@ -1,27 +1,38 @@
+import React, { useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+    const cityInputRef = useRef();
+
     const confirmHandler = (event) => {
         event.preventDefault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
     };
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <input ref={nameInputRef} type="text" id="name" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="street">Street</label>
-                <input type="text" id="street" />
+                <input ref={streetInputRef} type="text" id="street" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="postal">Postal Code</label>
-                <input type="text" id="postal" />
+                <input ref={postalCodeInputRef} type="text" id="postal" />
             </div>
             <div className={classes.control}>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city" />
+                <input ref={cityInputRef} type="text" id="city" />
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.onCancel}>
