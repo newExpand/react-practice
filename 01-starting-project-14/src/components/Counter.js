b45c6a4a21640c2604a6bd0f73a6a0a7e44887c0
newExpand <magin0auto@gmail.com>
2023-04-05 16:45:52 +0900

    feat: 내부 컴포넌트에서 action을 dispatch 하기

--- 01-starting-project-14/src/components/Counter.js
+++ 01-starting-project-14/src/components/Counter.js
@@ -1,14 +1,27 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+    const dispatch = useDispatch();
     const counter = useSelector((state) => state.count);
     const toggleCounterHandler = () => {};
+
+    const incrementHandler = () => {
+        dispatch({ type: "INCREMENT" });
+    };
+
+    const decrementHandler = () => {
+        dispatch({ type: "DECREMENT" });
+    };
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
             <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={incrementHandler}>증가</button>
+                <button onClick={decrementHandler}>감소</button>
+            </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
     );
