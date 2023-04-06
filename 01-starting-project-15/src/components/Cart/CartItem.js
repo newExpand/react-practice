f682b86b23d6344fb4fdca3a3ab482603bb033ef
newExpand <magin0auto@gmail.com>
2023-04-07 03:05:52 +0900

    feat: 리덕스 툴킷 복습2 장바구니 기능 완료

--- 01-starting-project-15/src/components/Cart/CartItem.js
+++ 01-starting-project-15/src/components/Cart/CartItem.js
@@ -1,18 +1,23 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-    const { title, quantity, total, price } = props.item;
-    const dispatch = useDispatch();
-    // const count = useSelector((state) => state.cart.totalQuantity);
+    const { id, title, quantity, total, price } = props.item;
 
+    const dispatch = useDispatch();
     const addCartItemHandler = () => {
-        dispatch(cartActions.addItemToCart(quantity));
+        dispatch(
+            cartActions.addItemToCart({
+                id,
+                title,
+                price,
+            })
+        );
     };
 
     const removeCartItemHandler = () => {
-        dispatch(cartActions.remove());
+        dispatch(cartActions.removeItemFromCart(id));
     };
 
     return (
