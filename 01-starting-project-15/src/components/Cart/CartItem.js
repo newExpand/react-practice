d4d3f45c49534a0268cf7a090ab78da6865dadea
newExpand <magin0auto@gmail.com>
2023-04-06 23:30:28 +0900

    feat: 리덕스 툴킷 복습하기 -store 만들기 및 장바구니 버튼 클릭시 ui 토글 기능

--- 01-starting-project-15/src/components/Cart/CartItem.js
+++ 01-starting-project-15/src/components/Cart/CartItem.js
@@ -1,28 +1,39 @@
-import classes from './CartItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+    const { title, quantity, total, price } = props.item;
+    const dispatch = useDispatch();
+    // const count = useSelector((state) => state.cart.totalQuantity);
 
-  return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
-    </li>
-  );
+    const addCartItemHandler = () => {
+        dispatch(cartActions.addItemToCart(quantity));
+    };
+
+    const removeCartItemHandler = () => {
+        dispatch(cartActions.remove());
+    };
+
+    return (
+        <li className={classes.item}>
+            <header>
+                <h3>{title}</h3>
+                <div className={classes.price}>
+                    ${total.toFixed(2)} <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+                </div>
+            </header>
+            <div className={classes.details}>
+                <div className={classes.quantity}>
+                    x <span>{quantity}</span>
+                </div>
+                <div className={classes.actions}>
+                    <button onClick={removeCartItemHandler}>-</button>
+                    <button onClick={addCartItemHandler}>+</button>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default CartItem;
