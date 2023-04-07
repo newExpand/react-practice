32ef093cc02e4458030695284b4ad1280d2691b9
newExpand <magin0auto@gmail.com>
2023-04-07 09:24:26 +0900

    feat: 리덕스와 useEffect 사용하기

--- 01-starting-project-15/src/App.js
+++ 01-starting-project-15/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,17 @@ import Products from "./components/Shop/Products";
 
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        fetch("https://react-http-f8c0c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(cart),
+        });
+    }, [cart]);
 
     return (
         <Layout>
