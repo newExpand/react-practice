78c5dddfd7cf426446947d3985bb72a4f449d276
newExpand <magin0auto@gmail.com>
2023-04-08 08:58:49 +0900

    feat: 객체가 아닌 JSX로 라우트를 설정하는 방법

--- 01-starting-project-16/src/App.js
+++ 01-starting-project-16/src/App.js
@@ -1,6 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
+
+// JSX코드로 라우트를 설정했던 방식(예전엔 이랬음)
+// const routeDefinitions = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/products" element={<Products />} />
+//     </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
