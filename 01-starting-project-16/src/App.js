857446931c50e70a014719bbf8fc85e6b2011d09
newExpand <magin0auto@gmail.com>
2023-04-08 08:51:46 +0900

    feat: 두번째 라우트 추가해보기

--- 01-starting-project-16/src/App.js
+++ 01-starting-project-16/src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import Products from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/products", element: <Products /> },
+]);
 
 function App() {
     return <RouterProvider router={router} />;
---
+++ 01-starting-project-16/src/pages/Products.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+    return <h1>상품페이지 이올시당~</h1>;
+};
+
+export default Products;
