fe8e6c90ebda96d7c505d90528224c4750588909
newExpand <magin0auto@gmail.com>
2023-04-07 18:11:37 +0900

    fix: 장바구니 아이템 삭제 후 새로고침 클릭시 나오는 오류 수정 refactor: 장바구니 정보 바뀔 시에만 렌더 가능하게 바꿈

--- 01-starting-project-15/src/store/cart-slice.js
+++ 01-starting-project-15/src/store/cart-slice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     items: [],
     totalQuantity: 0,
+    changed: false,
 };
 
 const cartSlice = createSlice({
@@ -18,6 +19,7 @@ const cartSlice = createSlice({
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             if (!existingItem) {
                 state.items.push({
                     id: newItem.id,
@@ -35,6 +37,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter((item) => item.id !== id);
             } else {
