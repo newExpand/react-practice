2d44abbf41f925878867f2bfa3ed8ec580b1c24d
newExpand <magin0auto@gmail.com>
2023-04-09 21:51:18 +0900

    feat: 동적 라우트에 링크 추가하기

--- 01-starting-project-16/src/pages/Products.js
+++ 01-starting-project-16/src/pages/Products.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const PRODUCT = [
+    { id: "p1", title: "상품1" },
+    { id: "p2", title: "상품2" },
+    { id: "p3", title: "상품3" },
+];
 
 const ProductsPage = () => {
     return (
         <>
             <h1>상품페이지 이올시당~</h1>
             <ul>
-                <li>상품1</li>
-                <li>상품2</li>
-                <li>상품3</li>
+                {PRODUCT.map((prod) => (
+                    <li key={prod.id}>
+                        <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
