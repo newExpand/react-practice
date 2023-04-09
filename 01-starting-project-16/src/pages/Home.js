bb7d88cbb444e61e8d6f273ad791a42363ec3d00
newExpand <magin0auto@gmail.com>
2023-04-09 19:10:58 +0900

    feat: 프로세스적으로 이동이 필요할 때 쓰이는 useNavigate 사용해 보기

--- 01-starting-project-16/src/pages/Home.js
+++ 01-starting-project-16/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 /* a태그 대신 Link를 사용하는 이유
     a태그를 사용하면 새로운 페이지를 로드하게 됨
     Link를 사용하면 SPA를 구현하면서 새로운 페이지를 로드하지 않고도 
@@ -7,11 +7,20 @@ import { Link } from "react-router-dom";
     페이지를 다시 로드하지 않고 더 나은 성능을 제공하기 때문
  */
 const Home = () => {
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate("/products");
+    };
+
     return (
         <>
             <div>나만의 홈페이지</div>
             <p>
                 가보자잇 <Link to="/products">상품을 보러!</Link>
+            </p>
+            <p>
+                <button onClick={navigateHandler}>네비게이션</button>
             </p>
         </>
     );
