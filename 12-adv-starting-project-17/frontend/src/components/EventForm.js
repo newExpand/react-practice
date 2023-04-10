8355aea4e44dcf230e13610ea2ed10509eeabda1
newExpand <magin0auto@gmail.com>
2023-04-11 02:37:37 +0900

    feat: 이벤트 생성데이터 백엔드에 제출 시 버튼에 제출 상태 나타내기

--- 12-adv-starting-project-17/frontend/src/components/EventForm.js
+++ 12-adv-starting-project-17/frontend/src/components/EventForm.js
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
     const navigate = useNavigate();
+    const navigation = useNavigation();
+
+    const isSubmitting = navigation.state === "submitting";
+
     function cancelHandler() {
         navigate("..");
     }
@@ -33,10 +37,10 @@ function EventForm({ method, event }) {
                 />
             </p>
             <div className={classes.actions}>
-                <button type="button" onClick={cancelHandler}>
+                <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
                     Cancel
                 </button>
-                <button>Save</button>
+                <button disabled={isSubmitting}>{isSubmitting ? "전송중..." : "Save"}</button>
             </div>
         </Form>
     );
