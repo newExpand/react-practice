b83b6248842da005a120a6d946f3c92164f05f5c
newExpand <magin0auto@gmail.com>
2023-04-10 16:46:40 +0900

    comment: loader로 데이터 불러올때 useNavigation으로 상태 체크하기

--- 12-adv-starting-project-17/frontend/src/pages/Root.js
+++ 12-adv-starting-project-17/frontend/src/pages/Root.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet /* useNavigation */ } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const Root = () => {
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === "loading" && <p>로딩중...</p>} */}
                 <Outlet />
             </main>
         </>
