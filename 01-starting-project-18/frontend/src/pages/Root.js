2ee5a3551ab38abe8f93410823d025ee9595d246
newExpand <magin0auto@gmail.com>
2023-04-11 22:14:59 +0900

    feat: 자동 로그아웃 구현하기

--- 01-starting-project-18/frontend/src/pages/Root.js
+++ 01-starting-project-18/frontend/src/pages/Root.js
@@ -1,19 +1,31 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+    // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+    const timeout = 3600;
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+    useEffect(() => {
+        if (!token) return;
+
+        setTimeout(() => {
+            submit(null, { method: "post", action: "/logout" });
+        }, 1 * 60 * 60 * 1000);
+    }, [token, submit]);
+
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+                <Outlet />
+            </main>
+        </>
+    );
 }
 
 export default RootLayout;
