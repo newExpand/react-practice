b3150f0f04b5069ade5b62e65c3d5ada5cdb5548
newExpand <magin0auto@gmail.com>
2023-04-11 19:45:50 +0900

    feat: 회원가입 정보 백엔드에 제출하기

--- 01-starting-project-18/frontend/src/pages/Authentication.js
+++ 01-starting-project-18/frontend/src/pages/Authentication.js
@@ -1,7 +1,39 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
-  return <AuthForm />;
+    return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export const action = async ({ request, params }) => {
+    const searchParams = new URL(request.url).searchParams;
+    const mode = searchParams.get("mode") || "login";
+
+    if (mode !== "login" && mode !== "signup") {
+        throw json({ message: "지원하지 않는 모드입니다." }, { status: 422 });
+    }
+
+    const data = await request.formData();
+    const authData = {
+        email: data.get("email"),
+        password: data.get("password"),
+    };
+
+    const response = await fetch("http://localhost:8080/" + mode, {
+        method: request.method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(authData),
+    });
+
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    }
+
+    if (!response.ok) throw json({ message: "사용자을 인증할 수 없습니다." }, { status: 500 });
+
+    return redirect("/");
+};
