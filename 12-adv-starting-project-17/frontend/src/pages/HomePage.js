8e4155f903942b8fcb96cf79712a0fa5e8e3b7b7
newExpand <magin0auto@gmail.com>
2023-04-11 15:42:44 +0900

    feat: 마무리 홈컴포넌트 작성

--- 12-adv-starting-project-17/frontend/src/pages/HomePage.js
+++ 12-adv-starting-project-17/frontend/src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PageContent from "../components/PageContent";
 
 const HomePage = () => {
-    return <div>HomePage</div>;
+    return (
+        <PageContent title="환영합니다!">
+            <p>놀라운 이벤트를 경험하세요!</p>
+        </PageContent>
+    );
 };
 
 export default HomePage;
