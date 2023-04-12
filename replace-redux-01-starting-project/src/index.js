fe903dad528fa21c1d4b3aa38cea0a0f645abeed
newExpand <magin0auto@gmail.com>
2023-04-13 00:42:42 +0900

    feat: 커스텀 훅 저장소 사용해서 기존 기능 채워넣기

--- replace-redux-01-starting-project/src/index.js
+++ replace-redux-01-starting-project/src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureStore from "./hooks-store/products-store";
 
+configureStore();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ProductsProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ProductsProvider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
