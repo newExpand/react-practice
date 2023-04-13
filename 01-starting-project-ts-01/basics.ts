4b51d37d07c706e02aee9a3ee45f684872d0f351
newExpand <magin0auto@gmail.com>
2023-04-14 07:45:33 +0900

    feat: 타입추론하기

--- 01-starting-project-ts-01/basics.ts
+++ 01-starting-project-ts-01/basics.ts
@@ -38,3 +38,9 @@ let people: {
     name: string;
     age: number;
 }[];
+
+// 타입 추론하기
+
+let course = "리액트 - 완벽 부수기";
+
+// course = 123; 에러 발생함. 타입 추론으로 :string을 표시하지 않아도 타입스크립트 자체에서 추론해서 course변수는 string이라 명시해줌
