7ec3c6d866031316cf72cb3292e30fae2dbc1812
newExpand <magin0auto@gmail.com>
2023-04-14 00:52:39 +0900

    feat: 기본정적 타입 작업하기

--- replace-redux-01-starting-project/src/index.js
+++ replace-redux-01-starting-project/src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import configureStore from "./hooks-store/products-store";
+import configureProductsStore from "./hooks-store/products-store";
 
-configureStore();
+configureProductsStore();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
