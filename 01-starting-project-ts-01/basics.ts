79c3da06b83bdc944c6ce38392fbf0bed934b26c
newExpand <magin0auto@gmail.com>
2023-04-14 07:35:20 +0900

    comment: 배열안의 객체 타입주기 주석 추가

---
+++ 01-starting-project-ts-01/basics.ts
@@ -0,0 +1,40 @@
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = "민수";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// 배열에 타입주기
+
+let hobbies: string[];
+
+hobbies = ["축구", "농구"];
+
+// 객체에 타입주기
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: "민수",
+    age: 32,
+};
+
+// person = {
+//     isEmployee: true
+// }
+
+// 배열 안의 객체에 타입주기
+
+let people: {
+    name: string;
+    age: number;
+}[];
