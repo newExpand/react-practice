e791dc4ee83dfd1393c4b65417031574a183ff21
newExpand <magin0auto@gmail.com>
2023-04-14 23:03:20 +0900

    chore: x타입스크립트 투두리스트 연습 환경설치

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todos = () => {
+    return (
+        <ul>
+            <li>리액트 배우기</li>
+            <li>타입스크립트 배우기</li>
+        </ul>
+    );
+};
+
+export default Todos;
