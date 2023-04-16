feb67eb7c1734ed10d3d989565014925d3450df8
newExpand <magin0auto@gmail.com>
2023-04-16 23:45:45 +0900

    feat: todo 제거 구현하기로 연습

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-    return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
+    // const [visible, setVisible] = useState(true);
+
+    // const deleteListHandler = () => {
+    //     setVisible((prev) => !prev);
+    // };
+    return (
+        <li onClick={props.onRemoveTodo} className={classes.item}>
+            {props.text}
+        </li>
+    );
 };
 
 export default TodoItem;
