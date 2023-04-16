271e5604d80bafaee91125a822b5eab69d4dbe1d
newExpand <magin0auto@gmail.com>
2023-04-16 20:36:40 +0900

    feat: useRef에 타입스크립트 적용해보기

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo = () => {
+    const todoTextInput = useRef<HTMLInputElement>(null);
+
     const submitHandler = (e: React.FormEvent) => {
         e.preventDefault();
+
+        const enteredText = todoTextInput.current!.value;
+
+        if (enteredText.trim().length === 0) {
+            return;
+        }
     };
 
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
-            <input type="text" id="text" />
+            <input type="text" id="text" ref={todoTextInput} />
             <button>Todo 추가하기</button>
         </form>
     );
